ad6fd6f4902c5db4daabc9c188fd4568c208f795
Mohammad Yasin <mohammadyasinbappy@outlook.com>
2025-08-09 00:27:03 +0600

    API is created

---
+++ server/api/movies.js
@@ -0,0 +1,24 @@
+export default defineEventHandler(() => {
+  return [
+    {
+      "id": 1,
+      "name": "Titanic",
+      "review": "This is dummy review 1"
+    },
+    {
+      "id": 2,
+      "name": "Iron Man",
+      "review": "This is dummy review 2"
+    },
+    {
+      "id": 3,
+      "name": "Superman",
+      "review": "This is dummy review 3"
+    },
+    {
+      "id": 4,
+      "name": "Avatar",
+      "review": "This is dummy review 4"
+    }
+  ]
+})
